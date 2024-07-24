58f8ff9d5cb3712670b06764d6f164e3abd0cfe2
Tuandicodedao <91586684+NguyenDucTuan404@users.noreply.github.com>
2024-07-25 02:39:21 +0700

    NextJs TailwindCSS

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg
+     dark:text-light dark:border-light sm:text-base'>
+      <Layout className='py-8 flex items-center justify-center lg:flex-col lg:py-6'>
+        <span>{new Date().getFullYear()} &copy; TuanDiCodeDao</span>
+      </Layout>
+    </footer>
+  )
+}
+
+export default Footer
